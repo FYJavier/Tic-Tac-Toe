b73ad947e5054a172eda70f1b2b5e9b32edf1c8f
Freya Javier <thesleepyunicorn63@gmail.com>
2022-07-22 13:12:09 -0500

    Added X's and O's animation

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var moves = Array(repeating: "", count: 9)
+    @State private var xTurn = true
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
@@ -19,24 +20,27 @@ struct ContentView: View {
                 ForEach(0..<9) { index in
                     ZStack {
                         Color.blue
-                        Text("X")
+                        Text(moves[index])
                             .font(.system(size:90))
                             .fontWeight(.heavy)
                     }
                     .frame(width: 120, height: 120, alignment: .center)
                     .cornerRadius(30)
                     .onTapGesture {
-                        moves[index] = "X"
+                        if moves[index] == "" {
+                            moves[index] = xTurn ? "X" : "O"
+                            xTurn.toggle()
+                        }
                     }
                 }
             })
+            .preferredColorScheme(.dark)
         }
-        .preferredColorScheme(.dark)
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
